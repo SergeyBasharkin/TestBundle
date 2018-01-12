80559229826e8eb25d28bc022591ecafca711832
sergey <rock-n-rolllll@bk.ru>
2018-01-12 13:10:26 +0300

    inject logger

--- Routing/RestLoader.php
+++ Routing/RestLoader.php
@@ -11,11 +11,15 @@ namespace Test\TestBundle\Routing;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Config\FileLocatorInterface;
 use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Router;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 class RestLoader extends Loader
 {
@@ -23,15 +27,17 @@ class RestLoader extends Loader
     private $loaded = false;
     private $container;
     private  $logger;
+    private $locator;
 
     /**
      * RestLoader constructor.
      * @param $container
      */
-    public function __construct(ContainerInterface $container, LoggerInterface $logger)
+    public function __construct(ContainerInterface $container, LoggerInterface $logger, FileLocatorInterface $fileLocator)
     {
         $this->logger=$logger;
         $this->container = $container;
+        $this->locator = $fileLocator;
     }
 
 
@@ -42,11 +48,20 @@ class RestLoader extends Loader
         }
 
         /** @var Router $router */
-        $router = $this->container->get('router');
+//        $router = $this->container->get('router');
         /** @var $collection \Symfony\Component\Routing\RouteCollection */
 
-        dump($router->match("/home"));
-        dump($router->match("/extra/"));
+        $path = $this->locator->locate($resource);
+        try {
+            $config = Yaml::parse(file_get_contents($path));
+        } catch (ParseException $e) {
+            throw new \InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML.', $path), 0, $e);
+        }
+        $collection = new RouteCollection();
+        $collection->addResource(new FileResource($path));
+
+        dump($config);
+        dump($collection);
 //
 //        $routes = $router->getRouteCollection();
 //        dump($router->getRouteCollection());
