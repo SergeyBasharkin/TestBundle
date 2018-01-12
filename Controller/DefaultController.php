060d3e7732f54ad4c9a40037a888f8c11a4a515a
sergey <rock-n-rolllll@bk.ru>
2018-01-12 13:48:11 +0300

    inject logger

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -11,15 +11,29 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-         $metadata = $this->getDoctrine()->getManager()->getMetadataFactory()->getAllMetadata();
+        $metadata = $this->getDoctrine()->getManager()->getMetadataFactory()->getAllMetadata();
 
         $choices = [];
-        foreach($metadata as $classMeta) {
+        foreach ($metadata as $classMeta) {
             $choices[] = $classMeta->getName();// Entity FQCN
             $choices["filelds"] = $this->getDoctrine()->getManager()->getClassMetadata($classMeta->getName())->getFieldNames();
         }
 
         // replace this example code with whatever you need
         return new Response(json_encode($choices));
+    }
+
+    public function listEntities($entity)
+    {
+        $metadata = $this->getDoctrine()->getManager()->getMetadataFactory()->getAllMetadata();
+        $repository = null;
+        foreach ($metadata as $classMetadata) {
+            $pathArray = explode("\\", $classMetadata->getName());
+            dump($pathArray);
+            if ($entity === $pathArray[count($pathArray) - 1]){
+                $repository = $this->getDoctrine()->getRepository($classMetadata->getName());
+                dump($repository);
+            }
+        }
     }
 }
--- Routing/RestLoader.php
+++ Routing/RestLoader.php
@@ -10,16 +10,11 @@ namespace Test\TestBundle\Routing;
 
 
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Config\FileLocatorInterface;
 use Symfony\Component\Config\Loader\Loader;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Router;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Yaml;
 
 class RestLoader extends Loader
 {
@@ -46,47 +41,18 @@ class RestLoader extends Loader
         if (true === $this->loaded) {
             throw new \RuntimeException('Do not add the "rest" loader twice');
         }
-
-        /** @var Router $router */
-//        $router = $this->container->get('router');
-        /** @var $collection \Symfony\Component\Routing\RouteCollection */
-
-        $path = $this->locator->locate($resource);
-
-        dump($path);
-        dump($resource);
-
-        try {
-            $config = Yaml::parse(file_get_contents($resource));
-        } catch (ParseException $e) {
-            throw new \InvalidArgumentException(sprintf('The file "%s" does not contain valid YAML.', $path), 0, $e);
-        }
-        $collection = new RouteCollection();
-        $collection->addResource(new FileResource($path));
-
-        dump($config);
-        dump($collection);
-//
-//        $routes = $router->getRouteCollection();
-//        dump($router->getRouteCollection());
-
-        //        // prepare a new route
-        $path = '/extra/';
-//        $defaults = array(
-//            '_controller' => 'App\Controller\ExtraController::extra',
-//        );
-//        $requirements = array(
-//            'id' => '\d+',
-//        );
-        $route = new Route($path, array('_controller' => 'Test\TestBundle\Controller\DefaultController::indexAction'));
-//
         $routes = new RouteCollection();
-//        // add the new route to the route collection
-        $routeName = 'extraRoute';
-        $routes->add($routeName, $route);
-//
+        $path = '/{entity}/{id}';
+        $listPath='/{entity}/';
+        $requirements = array(
+            'parameter' => '\d+',
+        );
+        $route = new Route($path, array('_controller' => 'Test\TestBundle\Controller\DefaultController::indexAction'), $requirements);
+        $routeList = new Route($listPath,array('_controller' => 'Test\TestBundle\Controller\DefaultController::listEntities'));
+        $routes->add('entityRoute', $route);
+        $routes->add('listEntitiesRoute', $routeList);
+
         $this->loaded = true;
-//        $this->logger->error($this->container->get('doctrine'));
 
         return $routes;
     }
