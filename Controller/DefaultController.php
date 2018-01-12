d2fd6f694b4687d2e00b0411c85afda7112410e9
sergey <rock-n-rolllll@bk.ru>
2018-01-12 14:22:58 +0300

    inject logger

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,9 +6,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
+
+    private $serializer;
+
+    /**
+     * DefaultController constructor.
+     */
+    public function __construct()
+    {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
     public function indexAction()
     {
         $metadata = $this->getDoctrine()->getManager()->getMetadataFactory()->getAllMetadata();
@@ -29,14 +47,11 @@ class DefaultController extends Controller
         $repository = null;
         foreach ($metadata as $classMetadata) {
             $pathArray = explode("\\", $classMetadata->getName());
-            dump($pathArray);
             if ($entity === $pathArray[count($pathArray) - 1]){
                 $repository = $this->getDoctrine()->getRepository($classMetadata->getName());
-                dump($repository);
             }
         }
 
-        dump($repository->findAll());
-        return new Response("hi");
+        return new Response($this->serializer->serialize($repository->findAll(), 'json'));
     }
 }
