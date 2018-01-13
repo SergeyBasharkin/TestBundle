b0e30bfdb515cfe8c98ede8a5c8a142c2fbbc7ca
sergey <rock-n-rolllll@bk.ru>
2018-01-14 02:31:35 +0300

    add blacklist

--- DependencyInjection/TestExtension.php
+++ DependencyInjection/TestExtension.php
@@ -6,9 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Routing\Router;
-use Test\TestBundle\Exception\MultipleRouteException;
-use Test\TestBundle\Helper\Utils;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -18,10 +15,6 @@ use Test\TestBundle\Helper\Utils;
 class TestExtension extends Extension
 {
     /**
-     * @var Router
-     */
-    private $router;
-    /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
@@ -29,38 +22,8 @@ class TestExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('test_rest', $config);
-        $this->router = $container->get(Router::class);
-        $routes = $this->router->getRouteCollection()->all();
-        $entitiesNames = $this->getListEntitiesNames($container);
-        if (!empty($entitiesNames)) {
-            $routeCollisions = array();
-            foreach ($routes as $key => $route) {
-                if ($key !== 'listEntitiesRoute' && $key !== 'entityRoute') {
-                    $explodedPath = Utils::parsePath($route->getPath());
-                    if (count($explodedPath) >= 3) continue;
-                    if (in_array($explodedPath[0], $entitiesNames)) {
-                        $routeCollisions[] = $route;
-                    }
-                    if (preg_match('/\{.*\}/', $explodedPath[0])) {
-                        $routeCollisions[] = $route;
-                    }
-                }
-            }
-        }
-        if (!empty($routeCollisions)) {
-            throw  new MultipleRouteException($routeCollisions);
-        }
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    public function getListEntitiesNames(ContainerBuilder $container){
-        $metadata = $container->get('doctrine')->getManager()->getMetadataFactory()->getAllMetadata();
-        $names = array();
-        foreach ($metadata as $classMetadata) {
-            $pathArray = explode("\\", $classMetadata->getName());
-            if (!in_array($classMetadata->getName(),$this->blacklist)) $names[] = $pathArray[count($pathArray) - 1];
-        }
-        return $names;
     }
 }
