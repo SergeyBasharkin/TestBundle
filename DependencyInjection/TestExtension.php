36853a4c5769b211e25800b4c331c2b407e7a759
sergey <rock-n-rolllll@bk.ru>
2018-01-14 02:26:11 +0300

    add blacklist

--- DependencyInjection/TestExtension.php
+++ DependencyInjection/TestExtension.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Test\TestBundle\Exception\MultipleRouteException;
+use Test\TestBundle\Helper\Utils;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -14,6 +16,7 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class TestExtension extends Extension
 {
+    private $router;
     /**
      * {@inheritdoc}
      */
@@ -22,8 +25,38 @@ class TestExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('test_rest', $config);
-
+        $this->router = $container->get('router');
+        $routes = $this->router->getRouteCollection()->all();
+        $entitiesNames = $this->getListEntitiesNames($container);
+        if (!empty($entitiesNames)) {
+            $routeCollisions = array();
+            foreach ($routes as $key => $route) {
+                if ($key !== 'listEntitiesRoute' && $key !== 'entityRoute') {
+                    $explodedPath = Utils::parsePath($route->getPath());
+                    if (count($explodedPath) >= 3) continue;
+                    if (in_array($explodedPath[0], $entitiesNames)) {
+                        $routeCollisions[] = $route;
+                    }
+                    if (preg_match('/\{.*\}/', $explodedPath[0])) {
+                        $routeCollisions[] = $route;
+                    }
+                }
+            }
+        }
+        if (!empty($routeCollisions)) {
+            throw  new MultipleRouteException($routeCollisions);
+        }
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function getListEntitiesNames(ContainerBuilder $container){
+        $metadata = $container->get('doctrine')->getManager()->getMetadataFactory()->getAllMetadata();
+        $names = array();
+        foreach ($metadata as $classMetadata) {
+            $pathArray = explode("\\", $classMetadata->getName());
+            if (!in_array($classMetadata->getName(),$this->blacklist)) $names[] = $pathArray[count($pathArray) - 1];
+        }
+        return $names;
     }
 }
